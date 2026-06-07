003421026377b96839636a95907aa9e3e781c671
Anandu Jith Panickar <user.name>
2026-06-07 17:35:48 +0530

    Day 4

---
+++ Day-4/string-manipulation.sql
@@ -0,0 +1,17 @@
+select concat("hello" ,' ',"world");
+use sql_practice;
+
+select concat(emp_name," works in ",department) from employees;
+
+select emp_name,length(emp_name) from employees;
+
+select upper(emp_name) from employees;
+
+select lower(emp_name) from employees;
+
+select substring(emp_name,1,3) from employees;
+
+select left(emp_name,4) from employees;
+select right(emp_name,4) from employees;
+
+select trim("  I AM BATMAN   ");
