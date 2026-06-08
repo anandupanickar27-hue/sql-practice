85181027d7fd986402fb1097414ad12ffb6c06bc
Anandu Jith Panickar <anandupanickar27@gmail.com>
2026-06-08 23:56:50 +0530

    rough

--- Day 5/case.sql
+++ Day 5/case.sql
@@ -3,3 +3,13 @@ select emp_name,salary,
 			case when salary>60000 then "high"
             else "normal"
             end as salar_category from employees;
+            
+SELECT emp_name,
+       salary,
+       CASE
+           WHEN salary >= 60000 THEN 'A'
+           WHEN salary >= 50000 THEN 'B'
+           WHEN salary >= 40000 THEN 'C'
+           ELSE 'D'
+       END AS grade
+FROM employees;
